fc5d0c5fa7b23ee17ffd2b9d1edbdb099a90a66d
FelizardoCaculo <felizardo1987@gmail.com>
2023-01-31 21:46:15 +0100

    Add Transactions templates

--- js/script.js
+++ js/script.js
@@ -12,6 +12,38 @@ const Modal = {
         document
             .querySelector('.modal-overlay')
             .classList.remove('active');
-
     }
 }
+
+const Transaction = {
+    incomes(
+        //Somar as Entradas
+    ){},
+    expenses(){
+        //Somar as Saídas
+    },
+    total(){
+        //Entradas - Saídas
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/02/2023',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/02/2023',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/02/2023',
+    },
+]
